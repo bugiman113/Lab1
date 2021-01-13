231053786cc60dfc1743a9135409a7e5412c4fae
bugiman113 <63304289+bugiman113@users.noreply.github.com>
2021-01-13 14:59:34 +0300

    Update Read_Write_file.c

--- untitled7/Read_Write_file.c
+++ untitled7/Read_Write_file.c
@@ -4,9 +4,9 @@ uint32_t account_xy(const uint64_t width, const uint64_t x, const uint64_t y);
 enum read_status bmp_read(FILE* in, struct image* img, struct bmp_header* bmpHeader1) {
     uint32_t mx = bmpHeader1->biWidth;
     uint32_t my = bmpHeader1->biHeight;
-    uint32_t mx3 = (3*mx+3) & (-4);
+    uint32_t mx3 = (3*mx) % (-4);
     size_t str;
-    //���뢠��� 䠩��
+    //áç¨âë¢ ­¨¥ ä ©« 
     for(uint32_t i = 0; i < my; ++i){
         uint32_t n= account_xy(mx, 0, i);
         str=fread(&img->data[n], sizeof(struct pixel), mx, in);
@@ -25,7 +25,7 @@ enum read_status bmp_read(FILE* in, struct image* img, struct bmp_header* bmpHea
 }//bmp_read
 
 enum write_status bmp_create(FILE* file, struct image img, uint32_t mx, uint32_t my){
-    uint32_t mx3 = (3*mx+3) & (-4);
+    uint32_t mx3 = (3*mx) % (-4);
     for (uint32_t j = 0; j < my; j++) {
         for (uint32_t i = 0; i < mx; i++) {
             uint32_t n= account_xy(mx, i, j);
